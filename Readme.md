15e1e02b5a5f85dc7138cd1755bf0516e5bca535
Ruben Guzman <rubendario981@hotmail.com>
2023-02-06 15:40:33 -0500

    Show some results into result page
    Some fake result to show in the result page were done, also was included the Readme.md in the project

---
+++ Readme.md
@@ -0,0 +1,45 @@
+### Google Clone
+##### This is a simple Google clone made with ReactJS. The project aims to recreate the Google homepage with basic functionality.
+
+
+#### Features
+Search bar for user input
+Display of search results
+Links to other Google services (e.g. Gmail, Google Drive, etc.)
+
+
+#### Getting Started
+##### Prerequisites
+<ul>
+<li>Node js</li>
+<li>npm</li>
+<li>API KEY to use the search engine Bing</li>
+</ul>
+
+
+#### Installation
+Clone the repository:
+bash
+Copy code
+git clone https://github.com/rubendario981/google-clone.git
+
+
+#### Install dependencies:
+##### You must get installed vite in your localmachine
+Go to folder google-clone and 
+> npm install
+> npm run dev
+
+Also the API KEY of the api bing must be include in the file .env
+VITE_API_KEY={YOUR API KEY FROM BING SEARCH ENGINE}
+
+
+#### Built With
+<ul>
+<li>ReactJS - A JavaScript library for building user interfaces</li>
+<li>Tailwind - A library for css styling the page </li>
+<li>Typescript - Strong types in the project</li>
+</ul>
+
+
+#### This project is licensed under the MIT License - see the LICENSE file for details.
--- src/Components/AllResults.tsx
+++ src/Components/AllResults.tsx
@@ -2,33 +2,51 @@ import { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom';
 import ico from "../assets/icon-google.png"
 const AllResults = () => {
-  type results = { title: string, url: string, description: string, id: number }
-  const [data, setData] = useState<results[]>();
+  type resultsInterface = { title: string, url: string, description: string, id: number }
+  const descFake = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptas, repellat totam expedita voluptates ab similique eum aperiam ipsum autem earum unde aspernatur voluptate iste, incidunt porro vero, laudantium commodi!!"
+  const simulateResults = [
+    { title: "Google Fake answer", url: "www.google.com", description: "This SPA is just the mockup the search engine of google, coming soon the project will be completed " + descFake, id: 1},
+    { title: "Bing search engine", url: "www.bing.com", description: descFake, id: 2},
+    { title: "BBC London new media", url: "www.bbc.com", description: descFake, id: 3},
+    { title: "CNN - Time warner company", url: "www.cnn.com", description: descFake, id: 4},
+    { title: "All news of France and world", url: "www.france24.com", description: descFake, id: 5},
+    { title: "Fox news - A Fox company news", url: "www.foxnews.com", description: descFake, id: 6},
+    { title: "Newspaper of financial news", url: "www.financialtimes.com", description: descFake, id: 7},
+    { title: "ESPN - The world wide leader on sports", url: "www.espn.com", description: descFake, id: 8},
+  ]
+  const [results, setResults] = useState<resultsInterface[]>(simulateResults);
   const location = useLocation();
+  
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const txtSearch = location.search.split(('='))[1]
+
   useEffect(() => {
     async function fetchData() {
-      const txtSearch = location.search.split(('='))[1]
-      const response = await fetch(`http://localhost:8080/api/search?query=${txtSearch}`);
-      setData(await response.json())
+      try{
+        const response = await fetch(`https://api.cognitive.microsoft.com/bing/v7.0/search?q=${txtSearch}&count=10&offset=0&mkt=es-us&safesearch=Moderate&apikey=${API_KEY}`);
+        const data = await response.json()
+        
+        data.code === 200 && setResults(await response.json())
+
+      } catch(e: any){
+        console.log("error", e)
+      }
     }
-    fetchData()
-    console.log("data", data)
+    fetchData()    
   }, [])
 
   return (
     <div className='mt-4'>
-      {data?.map((data, i) => (
+      {results?.map((data, i) => (
         <div key={i} className="mb-4 w-3/4">
-          <Link to={{ pathname: data.url }} target="_blank" >
+          <Link to={data.url} target="_blank" >
             <div className="flex">
-              <img src={ico} alt="ico page"  className='w-5 mr-2' />              
+              <img src={ico} alt="ico page" className='w-5 mr-2' />
               <p className="text-sm">{data.url}</p>
-
             </div>
             <h3 className='text-blue-800 font-semibold text-xl hover:underline'>{data.title}</h3>
           </Link>
-          {data.description} Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae accusantium iusto, dolor voluptate, molestias mollitia tempore maxime sit officiis assumenda omnis explicabo magni asperiores dolores tempora esse doloribus eveniet cumque.
-
+          {data.description}
         </div>
       ))}
     </div>
